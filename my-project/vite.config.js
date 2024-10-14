6da31a46dcfd03bd77c3907f99eacc894478c0c1
Olasunkanmi001 <dammyvintage@gmail.com>
2024-10-14 01:14:10 +0100

    fixing

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.JPG', '**/*.jpg'],
-})
+  server: {
+    historyApiFallback: true, // This will enable HTML5 History API support
+  },
+});
